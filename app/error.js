b34558ed61632cdb14514286208725f5eb9447b4
Abuzar hamad <ajmalleon23@gmail.com>
2026-06-27 02:10:03 +0500

    deploye error

--- app/error.js
+++ app/error.js
@@ -1,18 +1,23 @@
+
 "use client";
 
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
 
-export default function error({ error, reset }) {
-    useEffect(() => {
-        console.log(error)
+  return (
+    <div className="text-center mt-10">
+      <h1>Something went wrong. Please try again!</h1>
 
-    }, [error])
-
-    return (
-        <div className='text-center mt-10'>
-            <h1>Some thing went wrong. Please try again!</h1>
-            <button className='hover:text-amber-600' onClick={() => reset()}>Try Again</button>
-        </div>
-    )
+      <button
+        className="hover:text-amber-600"
+        onClick={() => reset()}
+      >
+        Try Again
+      </button>
+    </div>
+  );
 }
