2e6b7830eea0578b5e4364710a8b861258c49f30
Abuzar hamad <ajmalleon23@gmail.com>
2026-06-27 01:51:18 +0500

    Add about page

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='max-w-6xl mx-auto' >
+        <h2 className='font-bold text-2xl p-4  text-amber-200 '>About</h2>
+      <p className='text-lg '>Commodi, nihil. Officia dolorum nobis iusto assumenda quasi, magni nulla nihil saepe voluptates earum accusantium hic voluptate labore vero temporibus tempore id, minima expedita quod consequatur cum rerum reiciendis. Omnis in odit ipsam ex nihil, rerum officiis quod at quo, quaerat placeat culpa blanditiis reiciendis? Minus suscipit facere consequatur voluptatum sequi, eos repellendus dolor perferendis. Illum, excepturi odio. Voluptates, quidem? Atque eveniet eaque, accusamus officiis, eius adipisci saepe quas, distinctio modi dolores maiores nesciunt. Maiores tenetur dicta quibusdam, expedita itaque suscipit fugiat iusto dolore harum quod voluptas ad dignissimos voluptate error nihil. Magnam aperiam assumenda itaque, at voluptas neque veniam perspiciatis quia facere error ratione quas dolore tenetur sint placeat saepe. Deleniti quibusdam consequuntur laboriosam, odio doloremque commodi eveniet, ipsum vero voluptate perspiciatis necessitatibus molestias praesentium quis. Saepe similique sint corrupti repellendus nostrum? Amet aut culpa cumque inventore ex voluptatibus dicta accusantium mollitia quis facilis dolorum voluptatem, hic obcaecati! Debitis fugiat, placeat eligendi vel distinctio nobis temporibus soluta explicabo, earum repellendus in accusamus recusandae autem deleniti atque! Atque blanditiis esse distinctio, ab sint, repudiandae tenetur aliquam labore minima voluptatem veritatis, sequi obcaecati adipisci nisi inventore aperiam magni. Nobis dicta officia aliquam rerum necessitatibus vero quis asperiores itaque accusamus?</p>
+    </div>
+  )
+}
+
+export default page
