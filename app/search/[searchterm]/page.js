8e6d985284cd708e3868c68c93e38e496f808aa9
Abuzar hamad <ajmalleon23@gmail.com>
2026-06-27 03:52:30 +0500

    version 1

--- app/search/[searchterm]/page.js
+++ app/search/[searchterm]/page.js
@@ -1,29 +1,16 @@
-
-
-
 import Results from "@/components/Results";
 
 export default async function Page({ params }) {
   const { searchTerm } = await params;
 
-  const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
-  );
+  
+  console.log("searchTerm:", searchTerm);
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch search results");
-  }
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${encodeURIComponent(searchTerm)}&language=en-US&page=1&include_adult=false`
+  );
 
   const data = await res.json();
-  const results = data.results;
 
-  return (
-    <div>
-      {results.length === 0 ? (
-        <h1 className="text-center pt-6">No data found</h1>
-      ) : (
-        <Results results={results} />
-      )}
-    </div>
-  );
+  return <Results results={data.results} />;
 }
