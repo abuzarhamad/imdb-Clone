3569752ad2cf172b0da12f9ffb7970bba1577852
Abuzar hamad <ajmalleon23@gmail.com>
2026-06-27 02:23:13 +0500

    navbar edit

--- components/Navbaritems.js
+++ components/Navbaritems.js
@@ -1,23 +1,25 @@
-"use client"
+"use client";
 
-import React from 'react';
-import Link from 'next/link';
-import { useSearchParams } from 'next/navigation'
+import React from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export default function Navbaritems({ title, param }) {
-    const searchParam = useSearchParams();
-    const genre = searchParam.get("genre");
-    
+  const searchParam = useSearchParams();
+  const genre = searchParam.get("genre");
 
-    return (
-        <div>
-            <Link
-                className={`hover:text-amber-600 font-semibold ${genre === 
-                    param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : ''}`}
-
-                href={`/?genre=${param}`}>{title}</Link>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
 }
-
