a152980a750c1baef695bc692f2f8c8c162d4967
Abuzar hamad <ajmalleon23@gmail.com>
2026-06-27 02:27:25 +0500

    all okay

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Navbaritems from './Navbaritems'
+import React, { Suspense } from "react";
+import Navbaritems from "./Navbaritems";
 
 export default function Navbar() {
-
-    return (
-        <div className='flex gap-5 bg-gray-500 text-white py-3 justify-center '>
-            <Navbaritems title='Trending' param='fetchTrending ' />
-            <Navbaritems title='Top Rating' param='fetchTopRating ' />
-        </div>
-    )
+  return (
+    <div className="flex gap-5 bg-gray-500 text-white py-3 justify-center">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbaritems title="Trending" param="fetchTrending" />
+        <Navbaritems title="Top Rating" param="fetchTopRating" />
+      </Suspense>
+    </div>
+  );
 }
